d1f401aa217fc56fb4489d9ef265494056ca84cd
bgyorfi <bence.gyorfi119@gmail.com>
2025-04-25 11:31:16 +0200

    add static media url

--- photoalbum/urls.py
+++ photoalbum/urls.py
@@ -26,6 +26,4 @@ urlpatterns = [
     path('accounts/signup/', SignUpView.as_view(), name='signup'),
     path('', include('photos.urls'))
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
